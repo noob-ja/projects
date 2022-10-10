86963ed2ddd769a3fc0fac804163fd75a4e8c853
JA <noobja96@gmail.com>
2022-10-11 00:29:13 +0800

    Fix export error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/projects',
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['github.com'],
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+const config = require('../next.config');
+const title = "JA's Random Projects";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <base href={config.basePath} />
+        <link rel="icon" href="favicon.ico" />
+        <title>{title}</title>
+        <meta name="og:title" content={title} />
+        <meta name="description" content="Random projects" />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            title,
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
