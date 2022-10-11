142be2608455a7b2aaf66ed5cd40bc2b2402f2fa
JA <noobja96@gmail.com>
2022-10-11 19:43:00 +0800

    Change to use SWR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,7 @@
-import type { GetStaticProps, InferGetStaticPropsType } from 'next'
 import Image from 'next/future/image'
+import useSWR from 'swr'
 
 const username: string = 'noob-ja';
-
-export const getStaticProps: GetStaticProps = async () => {
-  const res: any = await fetch('https://api.github.com/users/' + username + '/repos?sort=asc');
-  const projectList: Array<Project> = (await res.json()).filter((project: Project) => {
-    return project.has_pages;
-  });
-  return {
-    props: {
-      projectList
-    }
-  };
-}
 
 declare type Project = {
   id?: number;
@@ -24,11 +12,18 @@ declare type Project = {
   has_pages?: boolean;
 };
 
-function Home({ projectList }: InferGetStaticPropsType<typeof getStaticProps>) {
+function Home() {
+  const { data } = useSWR('https://api.github.com/users/' + username + '/repos?sort=asc', (apiURL: string) => fetch(apiURL).then(res => res.json()));
+  if (!data) return "Loading...";
+  
+  const projectList: Array<Project> = data.filter((project: Project) => {
+    return project.has_pages;
+  });
+
   return (
     <div className="container mx-auto p-20 items-center text-center">
       <a href="https://github.com/noob-ja" target="_blank" rel="noopener noreferrer" className="text-3xl font-bold hover:underline">
-        <Image className="rounded-full inline-block mb-2" src="https://github.com/noob-ja.png" height={240} width={240} alt="noob-ja"></Image><br/>
+        <Image className="rounded-full inline-block mb-2" src="https://github.com/noob-ja.png" height={128} width={128} alt="noob-ja"></Image><br/>
         JA (noob-ja)
       </a>
       <hr className="border-dashed my-4" />
